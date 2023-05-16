4931e945f94b0210a7fd4911d17debb9f36b0edf
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-05-16 18:14:39 +0200

    mobile menu fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,8 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 function Hero() {
   return (
-    <div>
+    <div className="relative">
+      <div className="bg-black/30 absolute left-0 top-0 w-full h-full z-10" />
       <Image
         alt="dj_orhanito-hero"
         src="/action_hero.webp"
@@ -16,31 +17,29 @@ function Hero() {
         }}
       />
 
-      <div className="w-full relative z-20">
-        <div className="flex flex-col justify-center h-screen text-center text-white w-full max-w-[800px] mx-auto">
-          <motion.div
-            animate={{
-              scale: [1, 2, 2, 1, 1],
-              rotate: [0, 0, 180, 180, 0],
-              borderRadius: ["0%", "0%", "50%", "50%", "0%"],
-            }}
-            transition={{
-              duration: 2,
-              ease: "easeInOut",
-              times: [0, 0.2, 0.5, 0.8, 1],
-              // repeat: Infinity,
-              repeatDelay: 1,
-            }}
-          >
-            <h1 className="text-6xl mt-[-96px] font-bold text-red-300">
-              Hi, ich bin Orhan
-            </h1>
-            <p className="mt-10 text-2xl font-bold">Dein DJ für Dein Event</p>
-          </motion.div>
-          <button className="bg-red-300 text-black p-3 rounded-md w-[300px] mt-10 mx-auto">
-            <a href="#bookings">Anfrage</a>
-          </button>
-        </div>
+      <div className="relative z-20 flex flex-col h-screen justify-center text-center text-white w-full max-w-[800px] mx-auto">
+        <motion.div
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 180, 180, 0],
+            borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            // repeat: Infinity,
+            repeatDelay: 1,
+          }}
+        >
+          <h1 className="text-6xl  font-bold text-red-300">
+            Hi, ich bin Orhan
+          </h1>
+          <p className="mt-10 text-2xl font-bold">Dein DJ für Dein Event</p>
+        </motion.div>
+        <button className="bg-red-300 text-black p-3 rounded-md w-[300px] mt-10 mx-auto">
+          <a href="#bookings">Anfrage</a>
+        </button>
       </div>
     </div>
   );
